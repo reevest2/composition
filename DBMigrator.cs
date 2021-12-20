b995290f69b6cd5b74bdd94ce783ea61d4882f9d
reevest2 <reevest2@gmail.com>
2021-12-20 17:31:24 -0500

    initial

---
+++ DBMigrator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace composition
+{
+    class DbMigrator
+    {
+        private readonly Logger _logger;
+
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We are migrating blahhhh");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+            var dbMigrator = new DbMigrator(logger);
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
